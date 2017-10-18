10f335781e144a3b06de1968e78af198ed441eec
Simonarde Lima <simonarde@gmail.com>
2017-10-18 19:25:22 -0200

    resolução do mapa

--- atualizar_imagem.php
+++ atualizar_imagem.php
@@ -1,6 +1,28 @@
 <?php
 	if (isset($_POST)) {
-		// script para atualizar imagem...
+		if(isset($_FILES['imagem'])) {
+
+			$largura_permitida = 800;
+			$altura_permitida = 600;
+
+			list($largura, $altura) = getimagesize($_FILES['imagem']['tmp_name']);
+
+			$extensao = substr($_FILES['imagem']['name'],-4);
+			
+			if(strtolower($extensao) == '.jpg' || strtolower($extensao) == 'jpeg') {
+				if($largura == $largura_permitida && $altura == $altura_permitida) {
+					move_uploaded_file($_FILES['imagem']['tmp_name'], 'quebracabeca.jpg');
+					include 'gerar_quebracabeca.php';
+					header('Location: mostrar.php');
+				}
+				else {
+					echo '<h3>Erro: O tamanho da imagem precisa ser 800x600</h3>';
+				}
+			}
+			else {
+				echo '<h3>Erro: A extensão da imagem precisa ser .jpg</h3>';
+			}
+		}
 	}
 ?>
 <html>
@@ -8,7 +30,7 @@
 		<title>Atualizar Imagem</title>
 	</head>
 	<body>
-		<form action="atualizar_imagem_quebra_cabeca.php" action="POST">
+		<form action="atualizar_imagem.php" method="POST" enctype="multipart/form-data">
 			Enviar nova imagem para atualizar (Tamanho válido 800x600):
 			<br />
 			<input name="imagem" type="file" />
--- mostrar.php
+++ mostrar.php
@@ -1,9 +1,22 @@
+<?php
+	ini_set('display_errors', true);
+	
+	if(!isset($_GET['mostrar_solucao'])) {
+		$dir = 'imagens';
+		$pastas = array('..', '.');
+		$arquivos = array_diff(scandir($dir), $pastas);
+
+		shuffle($arquivos);
+	}
+
+?>
 <html>
 	<head>
 		<style>
 			*{padding:0; margin:0; border:0;}
 			table {border:0; border-spacing: 10px;}
 			table tr td{padding:0; margin:0}
+			.links{padding: 0 10px;}
 		</style>
 	</head>
 	<body>
@@ -11,10 +24,17 @@
 			<?php for($linha=1; $linha<=6; $linha++) : ?>
 			<tr>
 				<?php for($coluna=1; $coluna<=8; $coluna++) : ?>
-				<td><img src="imagens/<?php echo ($linha*8)-8+$coluna ?>.jpg" /></td>
+					<?php if(!isset($_GET['mostrar_solucao'])) : ?>
+						<td><img src="imagens/<?php echo array_shift($arquivos); ?>" /></td>
+					<?php else : ?>
+						<td><img src="imagens/<?php echo ($linha*8)-8+$coluna ?>.jpg" /></td>
+					<?php endif; ?>
 				<?php endfor; ?>
 			</tr>
 			<?php endfor; ?>
 		</table>
+		<div class="links">
+			<a href="atualizar_imagem.php">Alterar imagem</a> | <a href="mostrar.php">Embaralhar</a> | <a href="mostrar.php?mostrar_solucao=1">Mostrar solucionado</a>
+		</div>
 	</body>
 </html>
